40e41e13a4a7494fd7ba9b7561856fbf161bb5c9
Daerius-rs <dima.armawir@yandex.ru>
2021-11-16 02:45:57 +0300

    Add '.gitattributes' & rewrite all code (rework/redesign/refactoring)

--- PasswordGenerator/PasswordGenerator/MainWindow.xaml.cs
+++ PasswordGenerator/PasswordGenerator/MainWindow.xaml.cs
@@ -1,129 +1,256 @@
 ﻿using System;
-using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
+using PasswordGenerator.Generation;
 
 
 namespace PasswordGenerator
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        #region Fields
+        private static readonly object InstanceSyncRoot = new object();
+        private static volatile MainWindow _instance;
+        public static MainWindow Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (InstanceSyncRoot)
+                    {
+                        if (_instance == null)
+                            _instance = new MainWindow();
+                    }
+                }
 
-        private static StringBuilder _result = new StringBuilder();
-        private static Random _random = new Random();
-
-        private static bool _addUpperCase;
-        private static bool _addNumbers;
-        private static bool _addSymbols;
-        private static string _validChars = string.Empty;
-
-        #endregion
+                return _instance;
+            }
+        }
 
 
-        #region ClassLifeCycles
 
-        public MainWindow()
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        private readonly StringGenerator _stringGenerator;
+
+
+
+        private int _minLength;
+        public int MinLength
+        {
+            get
+            {
+                return _minLength;
+            }
+            set
+            {
+                if (value > MaxLength)
+                    value = MaxLength;
+
+                _minLength = value;
+                OnPropertyChanged(nameof(MinLength));
+
+                UpdateGenerationAccess();
+            }
+        }
+        private int _maxLength;
+        public int MaxLength
+        {
+            get
+            {
+                return _maxLength;
+            }
+            set
+            {
+                if (value < MinLength)
+                    value = MinLength;
+
+                _maxLength = value;
+                OnPropertyChanged(nameof(MaxLength));
+
+                UpdateGenerationAccess();
+            }
+        }
+        private int _length;
+        public int Length
+        {
+            get
+            {
+                return _length;
+            }
+            set
+            {
+                if (value < MinLength)
+                    value = MinLength;
+                if (value > MaxLength)
+                    value = MaxLength;
+
+                _length = value;
+                OnPropertyChanged(nameof(Length));
+
+                UpdateGenerationAccess();
+            }
+        }
+        private bool _includeLowerCaseCharacters;
+        public bool IncludeLowerCaseCharacters
+        {
+            get
+            {
+                return _includeLowerCaseCharacters;
+            }
+            set
+            {
+                _includeLowerCaseCharacters = value;
+                OnPropertyChanged(nameof(IncludeLowerCaseCharacters));
+
+                UpdateGenerationAccess();
+            }
+        }
+        private bool _includeUpperCaseCharacters;
+        public bool IncludeUpperCaseCharacters
+        {
+            get
+            {
+                return _includeUpperCaseCharacters;
+            }
+            set
+            {
+                _includeUpperCaseCharacters = value;
+                OnPropertyChanged(nameof(IncludeUpperCaseCharacters));
+
+                UpdateGenerationAccess();
+            }
+        }
+        private bool _includeDigits;
+        public bool IncludeDigits
+        {
+            get
+            {
+                return _includeDigits;
+            }
+            set
+            {
+                _includeDigits = value;
+                OnPropertyChanged(nameof(IncludeDigits));
+
+                UpdateGenerationAccess();
+            }
+        }
+        private bool _includeSpecialCharacters;
+        public bool IncludeSpecialCharacters
+        {
+            get
+            {
+                return _includeSpecialCharacters;
+            }
+            set
+            {
+                _includeSpecialCharacters = value;
+                OnPropertyChanged(nameof(IncludeSpecialCharacters));
+
+                UpdateGenerationAccess();
+            }
+        }
+        private bool _generationEnabled;
+        public bool GenerationEnabled
+        {
+            get
+            {
+                return _generationEnabled;
+            }
+            set
+            {
+                _generationEnabled = value;
+                OnPropertyChanged(nameof(GenerationEnabled));
+            }
+        }
+
+
+
+        private MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+
+            _minLength = int.MinValue;
+            _maxLength = int.MaxValue;
+            _length = 0;
+
+            MinLength = 4;
+            MaxLength = 30;
+            Length = MinLength;
+            IncludeLowerCaseCharacters = true;
+            IncludeUpperCaseCharacters = false;
+            IncludeDigits = false;
+            IncludeSpecialCharacters = false;
+
+            _stringGenerator = new StringGenerator(
+                MaxLength);
         }
 
 
-        #endregion
 
-        #region Methods
-
-        private static string GeneratePassword(int length)
+        private void UpdateGenerationAccess()
         {
-            if (_addUpperCase && _addNumbers && _addSymbols)
+            if (Length < 1
+                || Length < MinLength
+                || Length > MaxLength)
             {
-                _validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890?!@#$%^&*";
-            }
-            else if (_addUpperCase && _addNumbers)
-            {
-                _validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-            }
-            else if (_addUpperCase && _addSymbols)
-            {
-                _validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?!@#$%^&*";
-            }
-            else if (_addNumbers && _addSymbols)
-            {
-                _validChars = "abcdefghijklmnopqrstuvwxyz1234567890?!@#$%^&*";
-            }
-            else if (_addUpperCase)
-            {
-                _validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            }
-            else if (_addNumbers)
-            {
-                _validChars = "abcdefghijklmnopqrstuvwxyz1234567890";
-            }
-            else if (_addSymbols)
-            {
-                _validChars = "abcdefghijklmnopqrstuvwxyz?!@#$%^&*";
-            }
-            else
-            {
-                _validChars = "abcdefghijklmnopqrstuvwxyz";
+                GenerationEnabled = false;
+
+                return;
             }
 
-            while (0 < length--)
+            if (!(IncludeLowerCaseCharacters
+                 || IncludeUpperCaseCharacters
+                 || IncludeDigits
+                 || IncludeSpecialCharacters))
             {
-                _result.Append(_validChars[_random.Next((_validChars.Length))]);
+                GenerationEnabled = false;
+
+                return;
             }
-            return _result.ToString();
+
+            GenerationEnabled = true;
         }
 
-        #endregion
 
 
-        #region Events
-
-        private void btnGeneratePassword_Click(object sender, RoutedEventArgs e)
+        protected virtual void OnPropertyChanged(
+            [CallerMemberName] string propertyName = "")
         {
+            PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private void GeneratePasswordButton_Click(object sender, RoutedEventArgs e)
+        {
+            IsEnabled = false;
+
             try
             {
-                int length = int.Parse(comboBox.Text);
-                string generatedPassword = GeneratePassword(length);
-                showPassword.Content = generatedPassword;
+                var alphabets = PasswordGenerationAlphabet.None;
+
+                if (IncludeLowerCaseCharacters)
+                    alphabets |= PasswordGenerationAlphabet.LowerCaseCharacters;
+                if (IncludeUpperCaseCharacters)
+                    alphabets |= PasswordGenerationAlphabet.UpperCaseCharacters;
+                if (IncludeDigits)
+                    alphabets |= PasswordGenerationAlphabet.Digits;
+                if (IncludeSpecialCharacters)
+                    alphabets |= PasswordGenerationAlphabet.SpecialCharacters;
+
+                PasswordTextBox.Text = _stringGenerator.
+                    GeneratePassword(Length, alphabets);
             }
-            catch (Exception)
+            finally
             {
-                MessageBox.Show("Please select a password length.",
-                    "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                IsEnabled = true;
             }
         }
-
-        private void uppercaseCheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            _addUpperCase = true;
-        }
-
-        private void uppercaseCheckBox_Unchecked(object sender, RoutedEventArgs e)
-        {
-            _addUpperCase = false;
-        }
-
-        private void numbersCheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            _addNumbers = true;
-        }
-
-        private void numbersCheckBox_Unchecked(object sender, RoutedEventArgs e)
-        {
-            _addNumbers = false;
-        }
-
-        private void symbolsCheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            _addSymbols = true;
-        }
-
-        private void symbolsCheckBox_Unchecked(object sender, RoutedEventArgs e)
-        {
-            _addSymbols = false;
-        }
-
-        #endregion
     }
 }
